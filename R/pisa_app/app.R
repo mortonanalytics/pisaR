ab4b0b135a9faa8eb223fee6b65455abf5382358
Ryan Morton <mortonanalytics@gmail.com>
2018-03-30 16:24:38 -0600

    Added navbar to app

--- R/pisa_app/app.R
+++ R/pisa_app/app.R
@@ -3,36 +3,75 @@ library(shiny)
 library(dplyr)
 library(pisaR)
 # Define UI for application
-ui <- fluidPage(
+ui <- navbarPage(
 
    # Application title
-   titlePanel("PISA"),
+   title = "Pandemic Influenza Severity Index (PISA)",
 
-   # Sidebar
-   sidebarLayout(
-      sidebarPanel(
-         selectInput(inputId = "data", label = "Select Metric", choices = c("Transmission", "Seriousness", "Impact"))
-      ),
+   # Transmission Tab
+   tabPanel(title = "Transmission",
+     # Sidebar
+     sidebarLayout(
+        sidebarPanel(
+           uiOutput(outputId = "year"),width = 3
+        ),
+        # main panel
+        mainPanel(pisaROutput("map"),
+                  pisaROutput("heatmap")
+        )
 
-      # main panel
-      mainPanel(
-        pisaROutput("map")
       )
-   )
+     ),
+   tabPanel(title = "Seriousness"),
+   tabPanel(title = "Impact"),
+   tabPanel(title = "Country Summary"),
+   tabPanel(title = "About")
 )
 
 # Define server logic
 server <- function(input, output,session) {
+
+  ## load data into the app
   source("./data_scripts/load_data.R")
 
-  output$map <- renderPisaR({
+  ## render UI elements using data available
+  output$year <- renderUI({
+    selectInput("year", "Select A Year", choices = year_ui, selected = max(year_ui))
+  })
+
+  filter_data <- reactive({
+    req(input$year)
     data_plot %>%
+      filter(Year.Code == input$year)
+  })
+
+  output$map <- renderPisaR({
       pisaR()%>%
       createLayer(layerType = "globalMap",
                   layerColor = list("green","yellow", "orange", "red", "darkred"),
                   layerLabel = "map",
-                  layerData = data_plot,
-                  layerMapping = list(z_var = input$data))
+                  layerData = filter_data(),
+                  layerMapping = list(color_var = "Transmission",
+                                      time_var = "Year_Week_number",
+                                      key_data = "ISO",
+                                      key_map = "ISO_3_CODE")) %>%
+      defineColorScale(color_palette = list("green","yellow", "orange", "red", "purple", "whitesmoke", "whitesmoke"),
+                       color_key = list("below", "Low", "Moderate", "High", "Extra-ordinary", "No Impact", ""))
+
+  })
+
+  output$heatmap <- renderPisaR({
+      pisaR() %>%
+      createLayer(layerType = "heatmap",
+                  layerColor = list("green","yellow", "orange", "red", "darkred"),
+                  layerLabel = "heat",
+                  layerData = filter_data(),
+                  layerMapping = list(x_var = 'Year_Week_number',
+                                      y_var = 'SOV_CODE',
+                                      z_var = "Transmission")) %>%
+      defineColorScale(color_palette = list("green","yellow", "orange", "red", "purple", "whitesmoke", "whitesmoke"),
+                       color_key = list("below", "Low", "Moderate", "High", "Extra-ordinary", "No Impact", ""))
+
   })
 }
 
--- R/pisa_app/data_scripts/load_data.R
+++ R/pisa_app/data_scripts/load_data.R
@@ -1,4 +1,16 @@
 df <- read.csv("./data/data.csv", stringsAsFactors = FALSE)
 
 data_plot <- df %>%
-  select(ISO, Code, SOV_CODE,Title, Sovereign,Impact, Transmission, Seriousness, Week.Code, Year.Code, Year_Week_number)
+  select(ISO,
+         Code,
+         SOV_CODE,
+         Title,
+         Sovereign,
+         Impact,
+         Transmission,
+         Seriousness,
+         Week.Code,
+         Year.Code,
+         Year_Week_number)
+
+year_ui <- sort(unique(df$Year.Code))
